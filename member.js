85308d84889f58b030bf3eafd39b79d60472d093
Daniel Melo <115378192+dmeloca@users.noreply.github.com>
2024-10-12 18:56:02 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  return [
+    { skill: 'HTML', level: 10 },
+    { skill: 'CSS', level: 8 },
+    { skill: 'JavaScript', level: 7 },
+    { skill: 'React', level: 6 },
+    { skill: 'Node', level: 5 },
+    { skill: 'Python', level: 4 },
+  ];
+}
